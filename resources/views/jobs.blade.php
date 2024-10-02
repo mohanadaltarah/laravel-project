d8babf2f12d124ebd0c7ef3218dc6365c5c895d8
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-10-02 12:14:53 +0300

    Create needed seeders for populating the database

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,14 +3,22 @@
         Jobs Page
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach($jobs as $job)
-            <li>
-                <a href="/jobs/{{$job['id']}}" class="text-blue-500 hover:underline">
+            <a href="/jobs/{{$job['id']}}" class="block px-4 py-6 border border-gray-200 rounded-lg ">
+                <div class="font-bold text-blue-500 text-ss">
+                    {{$job->employer->name}}
+                </div>
+                <div>
                     <strong>{{$job['title']}}:</strong> pays {{$job['salary']}} per year
-                </a>
-            </li>
+                </div>
+            </a>
+
         @endforeach
-    </ul>
+
+        <div>
+            {{$jobs->links()}}
+        </div>
+    </div>
 
 </x-layout>
