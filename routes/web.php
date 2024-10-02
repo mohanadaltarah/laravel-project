dbdf630775648c0966edca3702ea7a655cbf8f1f
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-10-02 15:46:13 +0300

    Create a job creation page with validation

--- routes/web.php
+++ routes/web.php
@@ -9,18 +9,39 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function (){
-    $jobs = Job::with('employer')->paginate(10);
-    return view('jobs', [
+    $jobs = Job::with('employer')->latest()->paginate(10);
+    return view('jobs.index', [
         'jobs'=> $jobs
     ]);
 });
 
+Route::get('/jobs/create', function (){
+    return view('jobs.create');
+});
 
 Route::get('/jobs/{id}', function ($id){
     $job = Job::find($id);
 
-    return view('job', ['job'=>$job]);
+    return view('job.show', ['job'=>$job]);
 });
+
+Route::post('/jobs', function (){
+    request()->validate([
+        'title'=>['required', 'min:3'],
+        'salary'=>['required']
+    ]);
+
+    Job::create(
+        [
+            'title'=> request('title'),
+            'salary'=> request('salary'),
+            'employer_id'=> 1
+            ]
+    );
+
+    return redirect('/jobs');
+});
+
 
 Route::get('/contact', function () {
     return view('contact');
