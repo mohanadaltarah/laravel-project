3444c390c6b094b9bd644238f09475300511be1e
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-10-03 12:44:43 +0300

    Build login and registration forms

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegisterUserController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
@@ -9,7 +11,10 @@ Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
 
+Route::get('/register', [RegisterUserController::class, 'create']);
+Route::post('/register', [RegisterUserController::class, 'store']);
 
-
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
 
 
